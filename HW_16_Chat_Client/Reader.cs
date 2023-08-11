3bf6c48a4a6dfb72dbf886a90c5a36a706fe9897
Oleksii Kryvchykov <olexey81@gmail.com>
2023-08-11 20:24:51 +0300

    HW_16

---
+++ HW_16_Chat_Client/Reader.cs
@@ -0,0 +1,36 @@
+﻿namespace HW_16_Chat_Client
+{
+    internal class Reader
+    {
+        private readonly StreamReader _reader;
+        private readonly Client _client;
+        public Reader(Client client)
+        {
+            _client = client;
+            _reader = new StreamReader(_client.stream);
+        }
+
+        public Task Read()
+        {
+            return Task.Run(() =>
+            {
+                string? line = null;
+                do
+                {
+                    line = _reader.ReadLine();
+                    if (line.StartsWith('0'))
+                    {
+                        _client.ClientName = line[1..];             
+                    }
+                    else
+                    {
+                        Console.ForegroundColor = ConsoleColor.Magenta;
+                        Console.WriteLine(line);                                
+                        Console.ForegroundColor = ConsoleColor.Green;
+                    }
+
+                } while (true);
+            });
+        }
+    }
+}
---
+++ HW_16_Chat_lib/MessageType.cs
@@ -0,0 +1,10 @@
+﻿namespace HW_16_Chat_lib
+{
+    public enum MessageType : byte
+    {
+        Common = 0,
+        System = 1,
+        Message = 2,
+        Private = 3,
+    }
+}
